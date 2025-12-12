2697374e4b84b0b0f05978542d2b960897e9af0a
Victor Hugo <vhlima1008@gmail.com>
2025-12-12 20:22:19 -0300

    feat(count-mentions): add user mention counting solution

---
+++ Python/3433_Count_Mentions_Per_User.py
@@ -0,0 +1,26 @@
+from typing import List
+
+class Solution:
+    def countMentions(self, numberOfUsers: int, events: List[List[str]]) -> List[int]:
+        mentions = [0] * numberOfUsers
+        back = [0] * numberOfUsers
+        events.sort(key = lambda e: (int(e[1]), e[0]=="MESSAGE"))
+
+        for responseType, time, data in events:
+            time = int(time)
+            if responseType == "OFFLINE":
+                back[int(data)] = time + 60
+                continue
+
+            for token in data.split():
+                if token == "ALL":
+                    for user in range(numberOfUsers):
+                        mentions[user] += 1
+                elif token == "HERE":
+                    for user in range(numberOfUsers):
+                        if time >= back[user]:
+                            mentions[user] += 1
+                else:  
+                    mentions[int(token[2:])] += 1
+
+        return mentions
